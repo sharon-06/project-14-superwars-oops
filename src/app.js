8f9ede7f00f6c89344fe9b023a855ce5f20c6230
sharon-06 <csharon4043@gmail.com>
2022-02-01 15:32:52 +0530

    sharon 102

--- src/app.js
+++ src/app.js
@@ -19,68 +19,76 @@ const PLAYERS = [
     // "Slayer",
     // "Vader",
     // "Slingo"
-];
-
-// Player Class
-class Player {
+  ];
+  
+  // Player Class
+  class Player {
     constructor(id, name, type) {
-        // Create member variables and assign values
-        // Type your code
-
+      this.id = id;
+      this.name = name;
+      this.type = type;
+      this.strength = this.getRandomStrength();
+      this.image = "images/super-" + (this.id + 1) + ".png";
     }
-
+  
     // getting random strength
     getRandomStrength = () => {
-        return Math.ceil(Math.random() * 100);
-    }
-
+      return Math.ceil(Math.random() * 100);
+    };
+  
     // Create a player for displaying
     view = () => {
-        // Accumulate HTML template
-        // Type your code here
-
-    
-        return player;
-    }
-}
-
-// Superwar Class
-class Superwar {
+      // Accumulate HTML template
+      let player = document.createElement("div");
+      player.classList.add("player");
+      player.setAttribute("data-id", this.id);
+      let image = document.createElement("img");
+      image.setAttribute("src", this.image);
+      let name = document.createElement("div");
+      name.textContent = this.name;
+      name.classList.add("name");
+      let strength = document.createElement("div");
+      strength.textContent = this.strength;
+      strength.classList.add("strength");
+      player.append(image, name, strength);
+      return player;
+    };
+  }
+  
+  // Superwar Class
+  class Superwar {
     constructor(players) {
-    // Create a field players 
-    // Use Map method to loop through players argument and create new players
-    // Type your code here
-
+      // Create a field players
+      this.players = players.map((pname, id) => {
+        let type = id % 2 == 0 ? "hero" : "villan";
+        return new Player(id, pname, type);
+      });
     }
-
+  
     // Display players in HTML
     viewPlayers = () => {
-        let team = document.getElementById('heroes');
-        team.innerHTML = '';
-        let fragment =
-            this.buildPlayers('hero');
-        team.append(fragment);
-
-        team = document.getElementById('villains');
-        team.innerHTML = '';
-        fragment =
-            this.buildPlayers('villain');
-        team.append(fragment);
-    }
-
-    // Build players fragment 
+      let team = document.getElementById("heroes");
+      team.innerHTML = "";
+      let fragment = this.buildPlayers("hero");
+      team.append(fragment);
+  
+      team = document.getElementById("villains");
+      team.innerHTML = "";
+      fragment = this.buildPlayers("villain");
+      team.append(fragment);
+    };
+  
+    // Build players fragment
     buildPlayers = (type) => {
-        let fragment = document.createDocumentFragment();
-        this.players
-            .filter(player => player.type == type)
-            .forEach(player => fragment.append(player.view()));
-        return fragment;
-    }
-
-}
-
-
-window.onload = () => {
+      let fragment = document.createDocumentFragment();
+      this.players
+        .filter((player) => player.type == type)
+        .forEach((player) => fragment.append(player.view()));
+      return fragment;
+    };
+  }
+  
+  window.onload = () => {
     const superwar = new Superwar(PLAYERS);
     superwar.viewPlayers();
-}
+  };
